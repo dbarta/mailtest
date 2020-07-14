b550eb3f32f52acaf44f441b4872f955620a83b0
David Barta <david.barta@emc.com>
2020-07-14 17:57:39 +0300

    first commit

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,10 @@
 class NotificationMailer < ApplicationMailer
+    def welcome(recipient)
+        @recipient = recipient
+        @url  =  url_for(controller: 'recipients',action: 'index')
+
+        mail(:to => recipient.email,
+             :from =>"mailtest support<david@odeca.net>",
+             :subject => "Testing if email is working")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'recipients#index'
+  post 'recipients/:id/send_email', to: 'recipients#send_email', as: 'email_recipient'
+  resources :recipients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
